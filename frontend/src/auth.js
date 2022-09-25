cbe261269c668efcd999e07389b6e2538bf2003b
Maxim Konstantinov <konstantinovmax@gmail.com>
2022-09-25 18:27:51 +0300

    docs: Update readme

--- frontend/src/auth.js
+++ frontend/src/auth.js
@@ -1,47 +1,47 @@
 export const BASE_URL = 'https://api.mestomk.students.nomoredomains.monster';
 
 export const register = (email, password) => {
-    return fetch(`${BASE_URL}/signup`, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({email, password})
+  return fetch(`${BASE_URL}/signup`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  })
+    .then((res) => {
+      return res.json();
     })
     .then((res) => {
-        return res.json();
-    })
-    .then((res) => {
-        return res;
+      return res;
     })
     .catch((err) => console.log(err));
 };
 
 export const authorize = (email, password) => {
-    return fetch(`${BASE_URL}/signin`, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({email, password})
-    })
-    .then((res => res.json()))
-    .catch(err => console.log(err));
+  return fetch(`${BASE_URL}/signin`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  })
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
 };
 
 export const getContent = (token) => {
-    return fetch(`${BASE_URL}/users/me`, {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`,
-        }
-    })
-    .then(res => res.json())
+  return fetch(`${BASE_URL}/users/me`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((res) => res.json())
     .catch((err) => {
-        console.log(err);
+      console.log(err);
     });
 };
